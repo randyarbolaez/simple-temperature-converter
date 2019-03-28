833760543093add9f4e71dc9daad9274a71d684d
Randy <randyarbolaez0@gmail.com>
2019-03-28 13:58:02 -0400

    very simple temperature converter, nothing special v2

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ let celsius = document.getElementById('celsius');
 let kelvin = document.getElementById('kelvin');
 
 function temperatureFromFahrenheit(temp) {
-  fahrenheit.innerHTML = `${temp}°F |
+  fahrenheit.innerHTML = `FAHRENHEIT | ${temp}°F |
                           ${(((temp - 32) * 5) / 9).toFixed(4)}°C |
                           ${(((temp - 32) * 5) / 9 + 273.15).toFixed(3)}°K`;
 }
 
 function temperatureFromCelsius(temp) {
-  celsius.innerHTML = `   ${temp * 1.8 + 32}°F |
+  celsius.innerHTML = `CELSIUS | ${temp * 1.8 + 32}°F |
                           ${temp}°C |
                           ${temp + 273.15}°K`;
 }
 
 function temperatureFromKelvin(temp) {
-  kelvin.innerHTML = `    ${(((temp - 273.15) * 9) / 5 + 32).toFixed(2)}°F |
+  kelvin.innerHTML = `    KELVIN |
+                          ${(((temp - 273.15) * 9) / 5 + 32).toFixed(2)}°F |
                           ${(temp - 273.15).toFixed(4)}°C |
                           ${temp}°K `;
 }
